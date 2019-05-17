6a2169c559a1875765fef3ea0ed47b601397db2f
David Freitag <freitagdavid@outlook.com>
2019-05-17 09:50:55 -0400

    cpu: Add JMP, JEQ, JNE definitions.

--- cpu.h
+++ cpu.h
@@ -29,21 +29,25 @@ enum alu_op {
 
 // These use binary literals. If these aren't available with your compiler, hex
 // literals should be used.
-typedef void op(struct cpu *cpu, unsigned char operandA, unsigned char operandB);
+typedef void op(struct cpu *cpu, unsigned char operandA,
+                unsigned char operandB);
 
-#define ADD  0b10100000
-#define AND  0b10101000
+#define ADD 0b10100000
+#define AND 0b10101000
 #define CALL 0b01010000
-#define CMP  0b10100111
-#define DEC  0b01100110
-#define HLT  0b00000001
-#define LDI  0b10000010
-#define MUL  0b10100010
-#define POP  0b01000110
-#define PRN  0b01000111
+#define CMP 0b10100111
+#define DEC 0b01100110
+#define HLT 0b00000001
+#define INC 0b01100101
+#define JEQ 0b01010101
+#define JMP 0b01010100
+#define JNE 0b01010110
+#define LDI 0b10000010
+#define MUL 0b10100010
+#define POP 0b01000110
+#define PRN 0b01000111
 #define PUSH 0b01000101
-#define RET  0b00010001
-#define INC  0b01100101
+#define RET 0b00010001
 
 // TODO: more instructions here. These can be used in cpu_run().
 
